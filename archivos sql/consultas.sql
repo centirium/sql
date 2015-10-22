16221426587fec15235eacf65b3b3c830448052f
unknown <javier_lorda@ORD-4-9.CUATROVIENTOS.alumnos>
2015-10-22 17:06:02 +0200

    sql2

--- archivos sql/consultas.sql
+++ archivos sql/consultas.sql
@@ -32,5 +32,39 @@ order by sueldo, nombre_empl; /*ordenar los resultados    */
 select nombre_dep, ciudad_dep, avg(sueldo) as media_sueldo
 from empleados group by nombre_dep,ciudad_dep;
 
+select num_proyec,count(codigo_empl) as num_empleado from empleados
+group by num_proyec;
+
+select * from empleados;
+select num_proyec
+from empleados
+group by num_proyec
+having sum(sueldo) > 18000;
+
+select p.codigo_proyec,p.precio,c.nif,p.codigo_cliente
+from clientes c join proyectos p on c.codigo_cli where c.codigo_cli= 5;
+
+select codugo_empl, nombre_empl
+from empleados natural join departamentos
+where telefono = '935361345';
+
+
+select *
+from (empleados join proyectos on num_proyec=codigo_proyec)
+ join clientes on codigo_cliente=codigo_cli;
+ 
+
+select ciudad 
+from clientes union select ciudad_dep
+from departamentos;
+
+select c.ciudad from clientes c where c.ciudad in(select d.ciudad_dep from departamentos d 
+where c.ciudad=d.ciudad_dep);
+
+create table clientes2 like clientes;
+alter table clientes2 modify codigo_cli auto_increment;
+
+
+
 
 
